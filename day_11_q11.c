bb4ffbb055d31dcb51c3a3c99b1efb1e9945f290
manik55-lab <manik.23666@stu.upes.ac.in>
2026-02-11 14:29:48 +0530

    100 Days Of DSA

---
+++ day_11_q11.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main() {
+    int m, n;
+
+
+    scanf("%d %d", &m, &n);
+
+    int A[m][n], B[m][n], C[m][n];
+
+
+    for (int i = 0; i < m; i++) {
+        for (int j = 0; j < n; j++) {
+            scanf("%d", &A[i][j]);
+        }
+    }
+
+
+    for (int i = 0; i < m; i++) {
+        for (int j = 0; j < n; j++) {
+            scanf("%d", &B[i][j]);
+        }
+    }

5eccd7aa1bc77c9d8c12858abba49cd48e72f7d2
manik55-lab <manik.23666@stu.upes.ac.in>
2026-03-05 18:03:16 +0530

    100 Days Of DSA

---
+++ day_33_q33.c
@@ -0,0 +1,76 @@
+#include <stdio.h>
+#include <ctype.h>
+#include <string.h>
+
+#define MAX 100
+
+char stack[MAX];
+int top = -1;
+
+void push(char c) {
+    stack[++top] = c;
+}
+
+char pop() {
+    return stack[top--];
+}
+
+char peek() {
+    return stack[top];
+}
+
+int precedence(char op) {
+    if (op == '+' || op == '-')
+        return 1;
+    if (op == '*' || op == '/')
+        return 2;
+    return 0;
+}
+
+int main() {
+    char infix[MAX], postfix[MAX];
+    int i, j = 0;
+
+    scanf("%s", infix);
+
+    for (i = 0; i < strlen(infix); i++) {
+
+        // If operand → add to postfix
+        if (isalnum(infix[i])) {
+            postfix[j++] = infix[i];
+        }
+
+        // If '(' → push
+        else if (infix[i] == '(') {
+            push(infix[i]);
+        }
+
+        // If ')'
+        else if (infix[i] == ')') {
+            while (top != -1 && peek() != '(') {
+                postfix[j++] = pop();
+            }
+            pop(); // remove '('
+        }
+
+        // If operator
+        else {
+            while (top != -1 &&
+                   precedence(peek()) >= precedence(infix[i])) {
+                postfix[j++] = pop();
+                   }
+            push(infix[i]);
+        }
+    }
+
+    // Pop remaining operators
+    while (top != -1) {
+        postfix[j++] = pop();
+    }
+
+    postfix[j] = '\0';
+
+    printf("%s", postfix);
+
+    return 0;
+}

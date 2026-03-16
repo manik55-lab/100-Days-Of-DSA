534dec4589c4a19b2891a94a558f9f961ee8b202
manik55-lab <manik.23666@stu.upes.ac.in>
2026-03-16 17:36:48 +0530

    100 Days Of DSA

---
+++ day_37_q37.c
@@ -0,0 +1,80 @@
+#include <stdio.h>
+#include <string.h>
+
+#define MAX 100
+
+int pq[MAX];
+int size = 0;
+
+void insert(int x) {
+    if(size < MAX) {
+        pq[size++] = x;
+    }
+}
+
+int findMinIndex() {
+    if(size == 0)
+        return -1;
+
+    int minIndex = 0;
+
+    for(int i = 1; i < size; i++) {
+        if(pq[i] < pq[minIndex]) {
+            minIndex = i;
+        }
+    }
+
+    return minIndex;
+}
+
+void deleteElement() {
+    if(size == 0) {
+        printf("-1\n");
+        return;
+    }
+
+    int minIndex = findMinIndex();
+    printf("%d\n", pq[minIndex]);
+
+    for(int i = minIndex; i < size-1; i++) {
+        pq[i] = pq[i+1];
+    }
+
+    size--;
+}
+
+void peek() {
+    if(size == 0) {
+        printf("-1\n");
+        return;
+    }
+
+    int minIndex = findMinIndex();
+    printf("%d\n", pq[minIndex]);
+}
+
+int main() {
+    int N;
+    char op[10];
+    int x;
+
+    scanf("%d", &N);
+
+    for(int i = 0; i < N; i++) {
+
+        scanf("%s", op);
+
+        if(strcmp(op,"insert") == 0) {
+            scanf("%d",&x);
+            insert(x);
+        }
+        else if(strcmp(op,"delete") == 0) {
+            deleteElement();
+        }
+        else if(strcmp(op,"peek") == 0) {
+            peek();
+        }
+    }
+
+    return 0;
+}

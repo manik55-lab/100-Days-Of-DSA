87949b669034df8cc1b0b3772c76d487ec39a624
manik55-lab <manik.23666@stu.upes.ac.in>
2026-03-16 17:51:05 +0530

    100 Days Of DSA

---
+++ day_42_q42.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+
+#define MAX 100
+
+int queue[MAX];
+int front = 0, rear = -1;
+int stack[MAX];
+int top = -1;
+
+void enqueue(int x) {
+    queue[++rear] = x;
+}
+
+int dequeue() {
+    return queue[front++];
+}
+
+void push(int x) {
+    stack[++top] = x;
+}
+
+int pop() {
+    return stack[top--];
+}
+
+int main() {
+
+    int N, x;
+
+    scanf("%d", &N);
+
+    for(int i = 0; i < N; i++) {
+        scanf("%d", &x);
+        enqueue(x);
+    }
+
+    while(front <= rear) {
+        push(dequeue());
+    }
+
+    front = 0;
+    rear = -1;
+
+    while(top != -1) {
+        enqueue(pop());
+    }
+
+    for(int i = front; i <= rear; i++) {
+        printf("%d ", queue[i]);
+    }
+
+    return 0;
+}

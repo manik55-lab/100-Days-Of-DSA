1ad47b0ea55266cec5b88605539fd9cb627293c3
manik55-lab <manik.23666@stu.upes.ac.in>
2026-03-18 10:16:52 +0530

    100 Days Of DSA

---
+++ day_46_q46.c
@@ -0,0 +1,95 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// Tree Node
+struct Node {
+    int data;
+    struct Node* left;
+    struct Node* right;
+};
+
+// Create new node
+struct Node* createNode(int val) {
+    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+    newNode->data = val;
+    newNode->left = newNode->right = NULL;
+    return newNode;
+}
+
+// Build tree from level order input
+struct Node* buildTree(int arr[], int n) {
+    if (n == 0 || arr[0] == -1)
+        return NULL;
+
+    struct Node* root = createNode(arr[0]);
+
+    struct Node* queue[1000];
+    int front = 0, rear = 0;
+
+    queue[rear++] = root;
+    int i = 1;
+
+    while (i < n && front < rear) {
+        struct Node* current = queue[front++];
+
+        // Left child
+        if (arr[i] != -1) {
+            current->left = createNode(arr[i]);
+            queue[rear++] = current->left;
+        }
+        i++;
+
+        if (i >= n) break;
+
+        // Right child
+        if (arr[i] != -1) {
+            current->right = createNode(arr[i]);
+            queue[rear++] = current->right;
+        }
+        i++;
+    }
+
+    return root;
+}
+
+// Level Order Traversal
+void levelOrder(struct Node* root) {
+    if (root == NULL)
+        return;
+
+    struct Node* queue[1000];
+    int front = 0, rear = 0;
+
+    queue[rear++] = root;
+
+    while (front < rear) {
+        int size = rear - front;
+
+        for (int i = 0; i < size; i++) {
+            struct Node* temp = queue[front++];
+            printf("%d ", temp->data);
+
+            if (temp->left)
+                queue[rear++] = temp->left;
+            if (temp->right)
+                queue[rear++] = temp->right;
+        }
+        printf("\n"); // new level
+    }
+}
+
+int main() {
+    int n;
+    scanf("%d", &n);
+
+    int arr[n];
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    struct Node* root = buildTree(arr, n);
+
+    levelOrder(root);
+
+    return 0;
+}

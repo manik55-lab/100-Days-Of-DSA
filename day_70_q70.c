7ab6bb09c39ce51955d9a91a362415eba5ce9bfe
manik55-lab <manik.23666@stu.upes.ac.in>
2026-04-14 12:14:01 +0530

    100 Days Of DSA

---
+++ day_70_q70.c
@@ -0,0 +1,8 @@
+Problem: Compute shortest path from source and detect negative weight cycles using Bellman-Ford.
+
+Input:
+- n vertices
+- m edges (u,v,w)
+
+Output:
+- Shortest distances OR NEGATIVE CYCLE
